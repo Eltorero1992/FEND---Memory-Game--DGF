460c4e5ff1d55855ab7dc71035d02b84fe570a72
Daniel Gomez Firmat <DanielGFirmat@gmail.com>
2018-11-03 21:26:05 +0000

    Add createCardList function, to review open cards

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-let selectedCard = "";
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -32,8 +32,9 @@ Event listener to get card and card classes
 
  */
  document.addEventListener ('click',function(event){
- 	selectedCard =event.target;
- 	flipCard(selectedCard)
+ 	let selectedCard = event.target;
+ 	flipCard (selectedCard);
+ 	createCardList (selectedCard);
 })
 
 
@@ -42,12 +43,36 @@ Event listener to get card and card classes
  */
 
  function flipCard (selectedCard) {
- 	selectedCard.classList.add("open","show")
 
+ 	selectedCard.classList.add("open","show");
  }
+
  /*
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ */
+
+ function createCardList (selectedCard) {
+
+ 	let cardList = document.querySelectorAll(".open",".show")
+ 	if (cardList.length === 2 && cardList[0].firstElementChild.classList.value === cardList[1].firstElementChild.classList.value){
+ 		console.log(true);
+ 	} else {
+ 		console.log(false);
+ 	}
+ 		
+
+
+
+ 	 /*
+	}
  *  - if the list already has another card, check to see if the two cards match
+ */
+
+ }
+
+
+
+ /*
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
